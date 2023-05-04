ec276a9feaf9c79f9dde4ed072fa84cb9b83eac3
Teja2008 <86116952+Teja2008@users.noreply.github.com>
2023-05-04 20:30:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ function setup(){
 function draw(){
     image(video, 0,0,400,380);
 }
+function start(){
+    objectDetector= ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML= "status= detecting objects" ; 
+    objectname= document.getElementById("objectname").value
+
+}
+function modelLoaded(){
+status= true;
+console.log("modelLoaded");
+}

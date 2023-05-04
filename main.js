a2f7f7dbcc3a0dec58cfce5349ef771540c78836
Teja2008 <86116952+Teja2008@users.noreply.github.com>
2023-05-04 20:15:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function setup(){
-    canvas= createCanvas(400,380)
- canvas.center()
+    canvas= createCanvas(400,380);
+ canvas.center();
+ video= createCapture(VIDEO);
+ video.size(400,380);
+ video.hide();
 
+}
+function draw(){
+    image(video, 0,0,400,380);
 }
